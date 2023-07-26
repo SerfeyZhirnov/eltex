35ee4c56abce2c52f6ec78ce4dd5eac6c3fd2883
cresswec <sergey.zhirnov.2003@mail.ru>
2023-07-26 20:47:30 +0700

    Add TCP local client-server connection

---
+++ Ex12/TCP/client.c
@@ -0,0 +1,36 @@
+#include "header.h"
+
+int main() {
+  char buf[256];
+  int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+  if (socket_fd == -1) {
+    perror("Error on socket creation: ");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_un server;
+  memset(&server, 0, sizeof(struct sockaddr_un));
+  server.sun_family = AF_LOCAL;
+  strncpy(server.sun_path, SOCKET_NAME, sizeof(server.sun_path) - 1);
+
+  int ret = connect(socket_fd, (const struct sockaddr *)&server, sizeof(struct sockaddr_un));
+  if (ret == -1) {
+    perror("Error on connect server to client: ");
+    exit(EXIT_FAILURE);
+  }
+  
+  strcpy(buf, "Hello");
+  if (send(socket_fd, buf, 256, 0) == -1) {
+    perror("Erorr on client send message: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if(recv(socket_fd, buf, 256, 0) == -1) {
+    perror("Error on client recv message: ");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Message from server: %s\n", buf);
+
+  return EXIT_SUCCESS;
+}
---
+++ Ex12/TCP/server.c
@@ -0,0 +1,48 @@
+#include "header.h"
+
+int main() {
+  char buf[256];
+  struct sockaddr_un server, client;
+  int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+  if (socket_fd == -1) {
+    perror("Error on socket creation: ");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&server, 0, sizeof(struct sockaddr_un));
+  server.sun_family = AF_LOCAL;
+  strncpy(server.sun_path, SOCKET_NAME, sizeof(server.sun_path) - 1);
+  
+  if (bind(socket_fd, (const struct sockaddr *)&server, sizeof(struct sockaddr_un)) == -1) {
+    perror("Bind error: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(socket_fd, 5) == -1) {
+    perror("Error on start waiting connect: ");
+    exit(EXIT_FAILURE);
+  }
+
+  int client_fd = accept(socket_fd, NULL, NULL);
+  if (client_fd == -1) {
+    perror("Error on connect client to server: ");
+    exit(EXIT_FAILURE);
+  }
+
+  if(recv(client_fd, buf, 256, 0) == -1) {
+    perror("Error on server recv message: ");
+    exit(EXIT_FAILURE);
+  }
+  printf("Message from client: %s\n", buf);
+
+  strcpy(buf, "Hi");
+  if (send(client_fd, buf, 256, 0) == -1) {
+    perror("Erorr on server send message: ");
+    exit(EXIT_FAILURE);
+  }
+
+  close(client_fd);
+  unlink(SOCKET_NAME);
+  close(socket_fd);
+  return EXIT_SUCCESS;
+}
